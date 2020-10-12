988b2c78dc4a8f9bcf700346297df750df77f181
Dang Vu Nhat Quang <ghitacluber@gmail.com>
2020-10-12 15:44:55 +0700

    Task-4.1

---
+++ app/controllers/api/v1/authentication_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::AuthenticationController < ApiController
+  before_action :authenticate_token!, only: :show
+
+  def create
+    user = User.find_by email: params[:email]
+    if user&.authenticate params[:password]
+      valid_token = JsonWebToken.encode id: user.id, is_host: user.is_host
+      render json: {success: true, token: valid_token}, status: :ok
+    elsif user.nil?
+      render json: {success: false, message: "Account not found"}, status: :not_found
+    else
+      render json: {success: false, message: "Incorrect password"}, status: :bad_request
+    end
+  end
+
+  def show
+    render :show
+  end
+end
